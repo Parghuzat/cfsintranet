29a4662365ef1c41ed626d838f32e432d1408cd3
Parghuzat <paguzati@hotmail.com>
2022-12-13 12:10:00 -0500

    news api

---
+++ pages/api/GetNews.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+export default function NewsFeed() {
+
+    const url = 'https://rss.cbc.ca/lineup/canada-toronto.xml';
+
+    axios.get(url)
+
+    return(
+
+
+    )
+}
