0dda9cd4cd7a3136b656e4c03b7847f9df4afe7e
Peter Kahar <90359579+Parghuzat@users.noreply.github.com>
2022-12-15 22:38:15 -0500

    function to find img link

--- components/newsFeed/NewsFeed.js
+++ components/newsFeed/NewsFeed.js
@@ -4,12 +4,21 @@ import { Box } from '@mui/material';
 
 export default function NewsFeed(props) {
     const data = props.data;
+    
+
+    function findImgUrl () {
+        const start = data.description.search(/src=/i) + 5;
+        const end = data.description.substring(start).search(/"/i) + start;
+        const link = data.description.substring(start,end);
+        return link;
+    }
 
     return (
         <Box>
             <li>
                 <a href={data.link} target='_blank'>{data.title}</a>
-                <img src={data.tumbnail}></img>
+                <img src={findImgUrl(data.description)}></img>
+                
             </li>
         </Box>
         
