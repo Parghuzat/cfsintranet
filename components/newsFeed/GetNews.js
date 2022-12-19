e27ac3302ea360948bbb5d6804a6171375279904
Parghuzat <paguzati@hotmail.com>
2022-12-19 15:00:40 -0500

    add padding to box

--- components/newsFeed/GetNews.js
+++ components/newsFeed/GetNews.js
@@ -19,37 +19,41 @@ export default function GetNews() {
     },[])
 
     return (
+
         <Box 
             sx={{
                 flexDirection: 'row',
                 display: 'inline-flex',
                 flexWrap: 'wrap'
-            }}>
-            {/* <img src={rssFeed.feed.image} ></img> */}
+            }}
+        >
+            {/* <img src={rssFeed.feed.image} ></img>  */}
            
             {newsItem.map((obj) => {
                 return ( 
                         <Box
-                        sx={{
-                            display: 'flex',
-                            flexDirection: 'row',
-                            alignContent: 'center',
-                            alignItems: 'center',
-                            pt: '20px',
-                            pb: '20px',
-                            width: "250px",
-                            boxShadow: 1,
-                            backgroundColor: 'background.paper',
-                            border: 1,
-                            borderColor: 'dark',
-                            borderRadius: 2,
-                            fontWeight: 'bold',
-                            fontSize: 16,
-                            textDecoration: 'none',            
-                        }}
+                            sx={{
+                                display: 'flex',
+                                flexDirection: 'column',
+                                alignContent: 'center',
+                                alignItems: 'center',
+                                mx: '20px',
+                                my: '20px',
+                                px: '10px',
+                                py: '10px',
+                                width: '200px',
+                                boxShadow: 1,
+                                backgroundColor: 'background.paper',
+                                border: 1,
+                                borderColor: 'dark',
+                                borderRadius: 2,
+                                fontWeight: 'bold',
+                                fontSize: 14,
+                                textDecoration: 'none',            
+                            }}
                         >
                             <a href={obj.link} target='_blank'>
-                                <img src={obj.thumbnail} width="240" height="auto"></img>
+                                <img src={obj.thumbnail} width="180" height="auto"></img>
                                 <p >{obj.title}</p>
                             </a>
                         </Box>
